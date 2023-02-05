279e12b73da34c97e0971e98e743e6f3a6de8b47
thesnakebite <alvaro@thesnakebite.es>
2023-02-05 10:00:29 +0100

    Filter products by category

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,11 +7,14 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $products = Product::query()
             ->select(['title', 'slug', 'category_id', 'image']) 
             ->with('category:id,title,slug')
+            ->when($request->input('categoria', function ($q, $categoryId) {
+                $q->where('id', $categoryId);
+            }))
             ->paginate();
 
         return view('products', compact('products'));
